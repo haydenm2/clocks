0c76b7b3c4c06c817e70dc160b476ae74804706d
Hayden <haydenmmorgan@gmail.com>
2019-07-13 15:42:24 -0600

    added stopwatch unit testing and other unit testing frameworks

--- scripts/stopwatch.py
+++ scripts/stopwatch.py
@@ -3,77 +3,120 @@
 import sys
 from datetime import time, datetime
 import time as t
-import curses
-import os
 
-class Timer:
+class Stopwatch:
     # Initialization function
     def __init__(self):
-        self.Name = 'Timer' #timer name
-        self.Time = [0,0,0,0] #[hour,minute,second,microseconds]
-        self.Diff = [0,0,0,0] #time until [hour,minute,second,microseconds]
-        self.on = False
+        self.Name = 'Stopwatch' #timer name
+        self.On = False #state of timer object
+        self.Complete = False #completion state
+        self.Time = [0,0,0,0] #[hour,minute,second,microseconds] current time
+        self.Diff = [0,0,0,0] #[hour,minute,second,microseconds] time until finish
+        self.Finish = [0,0,0,0] #[hour,minute,second,microseconds] time of finish
+        self.Length = [0,0,0,0] #[hour,minute,second,microseconds] length of timer
+        self.cReset = False #reset command flag
+        self.cPause = False #pause command flag
+        self.cStart = False #start command flag
+        self.cDest = False #destructor command flag
+        self.Show = True #show timer output flag
 
-    # SetTime
-    def StartTime(self):
-        start = [datetime.now().hour,datetime.now().minute,datetime.now().second,datetime.now().microsecond]
-        while(self.on):
-            
-            self.Diff = 
-            self.Time = [h,m,s,0]
-        
-
-    def StopTime(self)
-
-    # SetName
+    #Setter Functions
     def SetName(self,name):
         self.Name = name
-
-    # PrintTime
-    def PrintTime(self,Title,Time,hold):
-        if(hold):
-            print('[',Title,']: ',Time[0],':',Time[1],':',Time[2],':',Time[3],end='\r')
-            sys.stdout.flush()
-            t.sleep(0.005)
-        else:
-            print('[',Title,']: ',Time[0],':',Time[1],':',Time[2],':',Time[3])
-        
-    # Check
-    def Check(self):
-        self.PrintTime(self.Name,self.Time,False)
-
-    # TimeTo
-    def TimeTo(self,hold=False):
-        execute = True
-        while(hold or execute):
-            now = [datetime.now().hour,datetime.now().minute,datetime.now().second,datetime.now().microsecond]
-            self.Diff = [self.Time[0]-now[0],self.Time[1]-now[1],self.Time[2]-now[2],self.Time[3]-now[3]]
-            while(self.Diff[3]<0):
-                self.Diff[2]-=1
-                self.Diff[3]+=1000000
-            while(self.Diff[2]<0):
-                self.Diff[1]-=1
-                self.Diff[2]+=60
-            while(self.Diff[1]<0):
-                self.Diff[0]-=1
-                self.Diff[1]+=60
-            if(self.Diff[0]<0):
-                print('[',self.Name,']: ','Alarm Passed')
-                self.Time = [0,0,0,0]
-                hold=False
-            else:
-                ptitle = 'Time to ' + self.Name
-                self.PrintTime(ptitle,self.Diff,hold)
-                execute = False
-            
-
-    # Close
-    def Close(self):
-        print('[',self.Name,']: ','Deleting Alarm')
-        return
     
-    # Destructor
+    def SetLength(self,length):
+        self.Length = length
+
+    #Execution Functions
+    def Execute(self):
+        while(not(self.cDest)):
+            if(self.On):
+                self.CheckFlags()
+                if(self.Show):
+                    self.PrintTime()
+                self.Update()
+            else:
+                self.CheckFlags()
+            
+    def Update(self):
+        self.Time = [datetime.now().hour,datetime.now().minute,datetime.now().second,datetime.now().microsecond]
+        self.TimeTo()
+
+    def PrintTime(self):
+            print('[',self.Name,']: ',self.Diff[0],':',self.Diff[1],':',self.Diff[2],':',self.Diff[3],end='\r')
+            sys.stdout.flush()
+
+    def CheckFlags(self):
+        if(self.cPause):
+            self.Pause()
+            self.cPause = False
+        if(self.cReset):
+            self.Reset()
+            self.cReset = False
+        if(self.cStart):
+            self.Start()
+            self.cStart = False
+        t.sleep(0.05)
+
+    def CalcFinish(self):
+        self.Finish = [self.Time[0]+self.Length[0],self.Time[1]+self.Length[1],self.Time[2]+self.Length[2],self.Time[3]+self.Length[3]]
+        if(self.Finish[3]>1000000):
+            self.Finish[3]=self.Finish[3]%1000000
+            self.Finish[2]=self.Finish[2]+(self.Finish[3]//1000000)
+        if(self.Finish[2]>60):
+            self.Finish[2]=self.Finish[2]%60
+            self.Finish[1]=self.Finish[1]+(self.Finish[1]//60)
+        if(self.Finish[1]>60):
+            self.Finish[1]=self.Finish[1]%60
+            self.Finish[0]=self.Finish[0]+(self.Finish[0]//60)
+        if(self.Finish[0]>23):
+            self.Finish[0]=self.Finish[0]%24
+        return
+
+    def TimeTo(self):
+        self.Diff = [self.Finish[0]-self.Time[0],self.Finish[1]-self.Time[1],self.Finish[2]-self.Time[2],self.Finish[3]-self.Time[3]]
+        while(self.Diff[3]<0):
+            self.Diff[2]-=1
+            self.Diff[3]+=1000000
+        while(self.Diff[2]<0):
+            self.Diff[1]-=1
+            self.Diff[2]+=60
+        while(self.Diff[1]<0):
+            self.Diff[0]-=1
+            self.Diff[1]+=60
+        if(self.Diff[0]<0):
+            self.Diff = [0,0,0,0]
+            self.Complete = True
+            self.On = False
+            print('[',self.Name,']','Timer Complete!          ')
+        else:
+            return
+
+    def Start(self):
+        self.On = True
+        self.Time = [datetime.now().hour,datetime.now().minute,datetime.now().second,datetime.now().microsecond]
+        self.CalcFinish()
+
+    def Pause(self):
+        self.Length=self.Diff
+        self.On = False
+
+    def Reset(self):
+        self.Name = 'Stopwatch' #timer name
+        self.On = False #state of timer object
+        self.Complete = False #completion state
+        self.Diff = [0,0,0,0] #[hour,minute,second,microseconds] time until finish
+        self.Finish = [0,0,0,0] #[hour,minute,second,microseconds] time of finish
+        self.Length = [0,0,0,0] #[hour,minute,second,microseconds] length of timer
+        self.cReset = False #reset command flag
+        self.cPause = False #pause command flag
+        self.cStart = False #start command flag
+        self.cDest = False #destructor command flag
+        self.Show = True #show timer output flag
+        return
+
     def __del__(self):
+        print('[',self.Name,']: ','Deleting Timer')
         return
     
     
